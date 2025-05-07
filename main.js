0099d8ae5b0159fd922b31c060934f737c1231cb
Sulaiman-F <s.alfawzan18@gmail.com>
2025-05-07 13:51:31 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+let studint = [
+    {
+        fristname: "Sulaiman",
+        age: 23,
+        url: "./image.png",
+        gaender: "male"
+    },
+    {
+        fristname: "Ahmed",
+        age: 23,
+        url: "./img2.jpeg",
+        gaender: "male"
+
+    },
+    {
+        fristname: "nawal",
+        age: 23,
+        url: "./img3.jpeg",
+        gaender: "female"
+
+    },
+]
+
+let body = document.body
+
+
+studint.forEach((curr) => {
+    let img = document.createElement("img")
+    let fname = document.createElement("p")
+    let age = document.createElement("p")
+    img.src = curr.url
+    img.style.height = "20%"
+    img.style.width = "20%"
+    fname.innerText = curr.fristname
+    age.innerText = curr.age
+    if (curr.gaender === "male") {
+        fname.style.backgroundColor = "red"
+    } else {
+        fname.style.backgroundColor = "pink"
+    }
+    body.appendChild(fname)
+    body.appendChild(age)
+    body.appendChild(img)
+})
+
